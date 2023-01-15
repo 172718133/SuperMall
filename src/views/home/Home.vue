4079622331a7c50b2217bc9bb1f9f5bb6e750dc4
YiLong <2488174858@qq.com>
2023-01-15 10:23:33 +0800

    首页轮播图封装和使用

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,16 +3,26 @@
     <NavBar>
       <div slot="center">首页</div>
     </NavBar>
+    <Swiper>
+      <SwiperItem v-for="item in banner" :key="item.title">
+        <a :href="item.link">
+          <img :src="item.image" alt="">
+        </a>
+      </SwiperItem>
+    </Swiper>
   </div>
 </template>
 
 <script>
 import NavBar from '@/components/common/navbar/NavBar.vue'
+import { Swiper, SwiperItem } from '@/components/common/swiper'
 import { getHomeDataAPI } from '@/api/home.js'
 export default {
   name: 'my-home',
   components: {
-    NavBar
+    NavBar,
+    Swiper,
+    SwiperItem
   },
   data () {
     return {
@@ -26,6 +36,7 @@ export default {
     async getHomeData () {
       const { data: res } = await getHomeDataAPI()
       this.banner = res.data.banner.list
+      console.log(this.banner)
       this.keyword = res.data.keywords.list
       this.recommend = res.data.recommend.list
     }
