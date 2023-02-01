a81dbe3105cbc4f4ad5d0859401114a75babf468
YiLong <2488174858@qq.com>
2023-02-01 10:07:16 +0800

    scroll基本使用

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -1,5 +1,107 @@
 <template>
-  <div>
+  <div class="wrapper" ref="wrapper">
+    <ul>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+      <li>ffff</li>
+    </ul>
     <NavBar>
       <div slot="center">分类</div>
     </NavBar>
@@ -7,15 +109,31 @@
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 import NavBar from '@/components/common/navbar/NavBar.vue'
 export default {
   name: 'my-category',
   components: {
     NavBar
+  },
+  data () {
+    return {
+      scroll: null
+    }
+  },
+  mounted () {
+    this.scroll = new BScroll(this.$refs.wrapper, {
+      probeType: 2
+    })
   }
 }
 </script>
 
 <style lang="less" scoped>
-
+  .wrapper {
+    height: 200px;
+    background-color: antiquewhite;
+    margin-top: 44px;
+    overflow: hidden;
+  }
 </style>
